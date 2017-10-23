76ad41a41e008220c5406fdaeeeb4017cf6a4d34
Amund Engen Bogetvedt <amundeb@outlook.com>
2017-10-23 15:09:38 +0200

    A lot!!

--- GeneticCsp/Program.cs
+++ GeneticCsp/Program.cs
@@ -20,14 +20,18 @@ namespace GeneticCsp
 
             //Make initial random solutions
             solutions = InitSolutions(solutions);
-            //PrintMat(solutions);
-            //Find the Fitness of the solutions.
+            //Find the Fitness of the solutions
             fitness = FitnessFind(solutions, cspGraph, fitness);
+            //sort the solutions
+            SortMat(fitness, solutions);
+            Mate(solutions);
+            //Print the solutions
+            PrintMat(solutions);
 
-            for (int i = 0; i < fitness.Length / 2; i++)
-            {
-                Console.WriteLine(fitness[i]);
-            }
+            //for (int i = 0; i < fitness.Length; i++)
+            //{
+            //    Console.WriteLine(fitness[i]);
+            //}
 
             Console.Read();
         }
@@ -49,14 +53,14 @@ namespace GeneticCsp
             return solutions;
         }
         static int[] FitnessFind(char[,] solutions, int[,] cspGraph, int[] fitness)
-        {
-            for(int solution = 0; solution < solutions.GetLength(0);solution++)
+        {           
+            for (int solution = 0; solution < solutions.GetLength(0);solution++)
             {
-                char color = solutions[solution, 0];
                 int fit = 0;
                 for(int i = 0; i < cspGraph.GetLength(0); i++)
                 {
-                    for(int j = 0;j<cspGraph.GetLength(1); j++)
+                    char color = solutions[solution, i];
+                    for(int j = i;j<cspGraph.GetLength(1); j++)
                     {
                         if (cspGraph[i, j] == 1 && solutions[solution, j].Equals(color))
                         {
@@ -69,6 +73,25 @@ namespace GeneticCsp
 
             return fitness;
         }
+        static void Mate(char[,] solutions)
+        {
+            Random rnd = new Random();
+            int rng1, rng2, child;
+
+            for (int i = 0; i < 16; i += 2)
+            {
+                child = (solutions.GetLength(0) / 2) + i;
+                rng1 = rnd.Next(solutions.GetLength(0));
+                do
+                    rng2 = rnd.Next(solutions.GetLength(0));
+                while (rng1 == rng2);
+
+                CopyRow(solutions, i, child);
+                CopyRow(solutions, i + 1, child + 1);
+
+
+            }
+        }
         static void PrintMat(char[,] solutions)
         {
             for (int i = 0; i < solutions.GetLength(0); i++)
@@ -80,6 +103,43 @@ namespace GeneticCsp
                     else
                         Console.Write("\n" + solutions[i, j]);
                 }
+            }
+        }
+        static void SortMat(int[] fitness, char[,] solutions)
+        {
+            int temp;
+            for(int i = 0; i < fitness.Length; i++)
+            {
+                for(int j = 1; j < fitness.Length; j++)
+                {
+                    if (fitness[j-1] > fitness[j])
+                    {
+                        temp = fitness[j-1];
+                        fitness[j-1] = fitness[j];
+                        fitness[j] = temp;
+                        solutions = SwapRow(solutions, j);
+                    }
+                }
+            }
+        }
+        static char[,] SwapRow(char [,] solutions, int swap)
+        {
+            char temp;
+
+            for(int i = 0; i<solutions.GetLength(1); i++)
+            {
+                temp = solutions[swap-1, i];
+                solutions[swap-1, i] = solutions[swap, i];
+                solutions[swap, i] = temp;
+            }
+
+            return solutions;
+        }
+        static void CopyRow(char[,] solutions, int from, int into)
+        {
+            for(int i = 0; i < solutions.GetLength(1);i++)
+            {
+                solutions[into, i] = solutions[from, i];
             }
         }
     }
